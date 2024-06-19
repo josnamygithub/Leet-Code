11b6f109be5160ffa5daeaa6fec274fa42eac61c
Josna Jose <131941980+josnamygithub@users.noreply.github.com>
2024-06-19 09:15:05 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (8.47%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -4,7 +4,7 @@ class Solution {
       int [] sum = new int[nums.length];
         sum[0] = nums[0];
         for(int i =1; i <nums.length ;i++){
-            sum[i] = sum[i-1]+ nums[i];
+            sum[i] = sum[i-1]+ nums[i ];
         }
         return sum;
     }
